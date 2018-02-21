18439b7042f6aa360998467ca187d4ca92e04dbb
AntLouiz <luizrodrigo46@hotmail.com>
2018-02-21 09:44:34 -0300

    using the scrapy.Request to access the category detail

--- coursera/spiders/coursera_spider.py
+++ coursera/spiders/coursera_spider.py
@@ -6,6 +6,7 @@ class CourseraSpiderSpider(scrapy.Spider):
     name = 'coursera_spider'
     allowed_domains = ['*']
     start_urls = ['https://pt.coursera.org/browse?languages=en']
+    root_url = 'https://www.coursera.org'
 
     def parse(self, response):
       categories_link = response.xpath("//a[contains(@class, 'DomainNavItem ')]")
@@ -15,6 +16,20 @@ class CourseraSpiderSpider(scrapy.Spider):
           "span[contains(@class, 'domain-name')]/text()"
         ).extract()
 
-        category_href = category.xpath("@href").extract()
+        category_href = category.xpath("@href").extract_first()
 
-        print(category_name, category_href)
+        self.log("{}{}".format(self.root_url, category_href))
+
+        request = scrapy.Request(
+          url="{}{}".format(self.root_url, category_href),
+          callback=self.parse_category,
+          dont_filter=True
+        )
+
+        self.log(request)
+
+        yield request
+
+    def parse_category(self, response):
+      self.log("ENTROU NO CALLBACK")
+      self.log(response.xpath('//title/text()').extract_first())
